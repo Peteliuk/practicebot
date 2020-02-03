cfea728260a4743b7c2549624441db11aa00c65d
Peteliuk <43549721+Peteliuk@users.noreply.github.com>
2020-02-03 20:23:26 +0200

    Update views.py
    Create registration functionality

--- bot/views.py
+++ bot/views.py
@@ -7,11 +7,16 @@ from rest_framework.views import APIView
 from telebot import TeleBot
 from telebot import types
 
+from emoji import emojize
+
 from .models import TelegramUser
 
 TOKEN = 'TOKEN'
+
 bot = TeleBot(TOKEN)
- 
+user = TelegramUser()
+
+grin = emojize(':grin:', use_aliases=True)
  
 class UpdateBot(APIView):
     def post(self, request):
@@ -23,40 +28,51 @@ class UpdateBot(APIView):
         return Response({'code': 200})
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['reg', 'register', 'start'])
 def start(message):
-	user = TelegramUser()
-	username = ''
-	user_password = ''
-	run = True
-	chat_id = message.chat.id
-	bot.send_message(chat_id, "Hello World!\n To sign up, enter your username:")
-	while run:
-		run = False
+	bot.send_message(message.chat.id, "Hello World!\n\n")
+	msg = bot.reply_to(message, "To sign up, enter your username:")
+	bot.register_next_step_handler(msg, sign_up_username)
+
+def sign_up_username(message):
+	try:
 		if message.text != message.from_user.username:
-			bot.send_message(chat_id, "Enter YOUR Telegram username!!!")
-			run = True
-	username = message.text
-	bot.send_message(chat_id, "Great! And the last enter your new password:")
-	new_password = message.text
-	bot.send_message(chat_id, "Confirm password:")
-	while run:
-		run = False
-		if message.text != new_password:
-			bot.send_message(chat_id, "Password unconfirmed! Try again!")
-			run = True
-	user_password = new_password
-	user.user_id = chat_id
-	user.username = username
-	user.user_password = user_password
-	user.save()
-	bot.send_message(chat_id, "Congratulation! You are registered! Have a nice day!")
+			msg = bot.reply_to(message, "Enter your TELEGRAM username")
+			bot.register_next_step_handler(msg, sign_up_username)
+		else:
+			user.username = message.text
+			msg = bot.reply_to(message, "Great! Now enter your new password")
+			bot.register_next_step_handler(msg, sign_up_password)
+	except Exception as e:
+		bot.reply_to(message, 'some error')
+		print(e)
 
+def sign_up_password(message):
+	try:
+		user.user_password = message.text
+		msg = bot.reply_to(message, "OK! Confirm your password")
+		bot.register_next_step_handler(msg, sign_up_confirm_password)
+	except Exception as e:
+		bot.reply_to(message, 'some error')
+		print(e)
 
-@bot.message_handler(func=lambda message: True, content_types=['audio', 'video', 'document', 'text', 'location', 'contact', 'sticker'])
-def default_command(message):
-	bot.reply_to(message, "Your message")
-
+def sign_up_confirm_password(message):
+	try:
+		if message.text != user.user_password:
+			msg = bot.reply_to(message, "Password unconfirmed! Try again!")
+			bot.register_next_step_handler(msg, sign_up_confirm_password)
+		else:
+			user.user_id = message.from_user.id
+			user.save()
+			bot.send_message(message.chat.id, f'Congratulation! You\'re registered {grin}')
+	except Exception as e:
+		bot.reply_to(message, 'some error')
+		print(e)
 
 # Webhook
-#bot.set_webhook(url="http://127.0.0.1:8000/")
+#bot.set_webhook(url="https://myapp.pythonanywhere.com/" + TOKEN)
+
+bot.enable_save_next_step_handlers()
+bot.load_next_step_handlers()
+
+bot.polling()

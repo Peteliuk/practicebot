5813b43361d10386af6a07f36c559c1d311d8d85
Peteliuk <43549721+Peteliuk@users.noreply.github.com>
2020-02-06 22:38:49 +0200

    Update views.py

--- bot/views.py
+++ bot/views.py
@@ -13,23 +13,14 @@ from .models import TelegramUser
 from .models import Task
 
 # Bot token
-TOKEN = 'TOKEN'
+TOKEN = 'Say my name ))'
 
 # Bot
 bot = TeleBot(TOKEN)
 
-# Modules
-user = TelegramUser() # Telegram Users Module
-
 # Emojis
 grin = emojize(':grin:', use_aliases = True)
-slightly_smilling_face = emojize(':slightly_smiling_face:', use_aliases = True)
-
-# Telegram user (for checking of exsisting)
-tguser = None
-
-# User's task
-task = None
+slightly_smiling_face = emojize(':slightly_smiling_face:', use_aliases = True)
 
 # For reading response text from telegram server
 class UpdateBot(APIView):
@@ -43,140 +34,47 @@ class UpdateBot(APIView):
 # Start command
 @bot.message_handler(commands=['start', 'Start'])
 def start(message):
-	text = f'Hello {message.from_user.first_name} and welcome here!\n\n'
-	text += 'To see some functions, type "/help" or "/Help"'
-	bot.send_message(message.chat.id, text)
+	# if user_id field equals None, telegram user isn't signed in
+	# if user_id field equals telegram user's id, he or she is signed in
+	tguser = TelegramUser.objects.filter(user_id=message.from_user.id).first()
+	if tguser:
+		text = f'Hello {message.from_user.first_name}, how are you? {slightly_smiling_face}\n'
+		text += 'To see more information, type "/help" or "/Help"'
+		bot.send_message(message.chat.id, text)
+	else:
+		text = f'Hello {message.from_user.first_name}, how are you? {slightly_smiling_face}\n'
+		text = f'To use the bot enter your login plz {slightly_smiling_face}:'
+		msg = bot.send_message(message.chat.id, text)
+		bot.register_next_step_handler(msg, sign_in_username)
 
 # Help command
 @bot.message_handler(commands=['help', 'Help'])
 def help(message):
-	key = types.InlineKeyboardMarkup(row_width=3)
-	key.add(types.InlineKeyboardButton('Sign Up', callback_data = 'sign_up'))
-	key.add(types.InlineKeyboardButton('Sign In', callback_data = 'sign_in'))
-	key.add(types.InlineKeyboardButton('Logout', callback_data = 'logout'))
-	text = f'What can I do for you {slightly_smilling_face}? Check some functions {grin}'
-	bot.send_message(message.chat.id, text, reply_markup=key)
-
-
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-	try:
-		# Telegram user (for checking of exsisting)
-		global tguser
-		tguser = TelegramUser.objects.filter(user_id = call.message.chat.id).first()
-		if tguser:
-			tguser.user_id = tguser.user_id
-			tguser.name = tguser.name
-			tguser.password = tguser.password
-			tguser.loginned = tguser.loginned
-			text = f'id: {tguser.user_id}\tname: {tguser.name}\n'
-			text += f'password: {tguser.password}\tloginned: {tguser.loginned}'
-			text += '\n----------------\n' 
-			print(text)
-		if call.message:
-			if call.data == 'sign_up':
-				if tguser:
-					bot.send_message(call.message.chat.id, 'You are registered')
-					if tguser.loginned:
-						bot.send_message(call.message.chat.id, 'and signed in')
-				else:
-					text = "To sign up, enter your username:"
-					msg = bot.send_message(call.message.chat.id, text)
-					bot.register_next_step_handler(msg, sign_up_username)
-			elif call.data == 'sign_in':
-				if tguser:
-					if tguser.loginned:
-						bot.send_message(call.message.chat.id, 'You are signed in.')
-					else:
-						text = "To sign in, enter your username:"
-						msg = bot.send_message(call.message.chat.id, text)
-						bot.register_next_step_handler(msg, sign_in_username)
-				else:
-					text = "There are no account at database! Sign up first!\n"
-					text += "To sign up, enter your username:"
-					msg = bot.send_message(call.message.chat.id, text)
-					bot.register_next_step_handler(msg, sign_up_username)
-			elif call.data == 'logout':
-				if tguser:
-					if tguser.loginned:
-						tguser.loginned = False
-						tguser.save()
-						bot.send_message(call.message.chat.id, 
-							f'You have made logout! {slightly_smilling_face}')
-					else:
-						bot.send_message(call.message.chat.id, 'You\'re already unsigned in!')
-				else:
-					text = "There are no account at database! Sign up first!\n"
-					text += "To sign up, enter your username:"
-					msg = bot.send_message(call.message.chat.id, text)
-					bot.register_next_step_handler(msg, sign_up_username)
-			else:
-				raise Exception()
-	except Exception as e:
-		bot.reply_to(call.message, 'some error')
-		print(repr(e))
-	
-# Sign Up
-def sign_up_username(message):
-	try:
-		if message.text != message.from_user.username:
-			msg = bot.reply_to(message, "Enter your TELEGRAM username")
-			bot.register_next_step_handler(msg, sign_up_username)
-		else:
-			user.name = message.text
-			msg = bot.reply_to(message, "Great! Now enter your new password")
-			bot.register_next_step_handler(msg, sign_up_password)
-	except Exception as e:
-		bot.reply_to(message, 'some error')
-		print(repr(e))
-
-def sign_up_password(message):
-	try:
-		user.password = message.text
-		msg = bot.reply_to(message, "OK! Confirm your password")
-		bot.register_next_step_handler(msg, sign_up_confirm_password)
-	except Exception as e:
-		bot.reply_to(message, 'some error')
-		print(repr(e))
-
-def sign_up_confirm_password(message):
-	try:
-		if message.text != user.password:
-			msg = bot.reply_to(message, "Password unconfirmed! Try again!")
-			bot.register_next_step_handler(msg, sign_up_confirm_password)
-		else:
-			user.user_id = message.from_user.id
-			user.loginned = True
-			user.save()
-			bot.send_message(message.chat.id, f'Congratulation! You\'re registered {grin}')
-	except Exception as e:
-		bot.reply_to(message, 'some error')
-		print(repr(e))
-
+	bot.send_message(message.chat.id, f'In development {grin}.')
 
 # Sign In
 def sign_in_username(message):
 	try:
-		if message.text != tguser.name:
-			msg = bot.reply_to(message, "Enter your TELEGRAM username")
-			bot.register_next_step_handler(msg, sign_in_username)
+		tguser = TelegramUser.objects.filter(login=message.text).first()
+		if tguser:
+			msg = bot.reply_to(message, "Great! Now enter your password:")
+			bot.register_next_step_handler(msg, lambda m: sign_in_password(m, tguser))
 		else:
-			msg = bot.reply_to(message, "Great! Now enter your password")
-			bot.register_next_step_handler(msg, sign_in_password)
+			msg = bot.reply_to(message, "Incorrect login! Try again:")
+			bot.register_next_step_handler(msg, sign_in_username)
 	except Exception as e:
 		bot.reply_to(message, 'some error')
 		print(repr(e))
 
-def sign_in_password(message):
+def sign_in_password(message, tguser):
 	try:
-		if message.text != tguser.password:
-			msg = bot.reply_to(message, "Incorrected password! Try again:")
-			bot.register_next_step_handler(msg, sign_in_password)
-		else:
-			tguser.loginned = True
+		if message.text == tguser.password:
+			tguser.user_id = message.from_user.id
 			tguser.save()
 			bot.send_message(message.chat.id, f'Congratulation! You\'re signed in {grin}')
+		else:
+			msg = bot.reply_to(message, "Incorrected password! Try again:")
+			bot.register_next_step_handler(msg, sign_in_password)
 	except Exception as e:
 		bot.reply_to(message, 'some error')
 		print(repr(e))
@@ -185,4 +83,4 @@ def sign_in_password(message):
 bot.enable_save_next_step_handlers()
 bot.load_next_step_handlers()
 
-bot.polling()
+#bot.polling()

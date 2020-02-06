cd818e81ca8e91818f1006fc1a0ed21e21014f74
Peteliuk <43549721+Peteliuk@users.noreply.github.com>
2020-02-06 18:19:24 +0200

    Update models.py
    Deleted all needless

--- bot/models.py
+++ bot/models.py
@@ -2,18 +2,17 @@ from django.db import models
 from django.utils import timezone
 
 class TelegramUser(models.Model):
-	user_id = models.CharField(max_length=200, unique=True)
-	name = models.CharField(max_length=200)
+	user_id = models.CharField(max_length=200, null=True, blank=True)
+	login = models.CharField(max_length=200, unique=True)
 	password = models.CharField(max_length=200)
-	loginned = models.BooleanField(default=False)
 
 	def __str__(self):
-		return self.name
+		return self.login
 
 
 class Task(models.Model):
 	name = models.CharField(max_length=300)
-	description = models.TextField()
+	description = models.TextField(default='No description')
 	date = models.DateTimeField(default=timezone.now)
 	user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE)
 	acceed = models.BooleanField(default=False)

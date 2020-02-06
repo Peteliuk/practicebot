017dfe78863068478544682b752f0f712a593b24
Peteliuk <43549721+Peteliuk@users.noreply.github.com>
2020-02-06 21:20:35 +0200

    Update admin.py
    Unregister default fields. Register Telegram User's fields and Tasks fields. Custim admin panel

--- bot/admin.py
+++ bot/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from .models import TelegramUser
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
 
-admin.site.register(TelegramUser)
+from .models import TelegramUser
+from .models import Task
+
+class TelegramUserAdmin(admin.ModelAdmin):
+	list_display = ('login', 'password', 'user_id')
+	search_fields = ('login',)
+
+class TaskAdmin(admin.ModelAdmin):
+	list_display = ('name',	'date',	'user', 'acceed', 'complited')
+	search_fields = ('name', 'user')
+
+admin.site.site_header = 'Панель адміністратора'
+
+admin.site.register(TelegramUser, TelegramUserAdmin)
+admin.site.register(Task, TaskAdmin)
+
+admin.site.unregister(Group)
+admin.site.unregister(User)

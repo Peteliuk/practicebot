1030059d495999a46c7a1ad88d9e8fd98e8aa1bd
Peteliuk <43549721+Peteliuk@users.noreply.github.com>
2020-02-10 20:12:46 +0200

    Update models.py
    Added status field with choices. Deleted "acceed" and "complited" fields.

--- bot/models.py
+++ bot/models.py
@@ -9,14 +9,19 @@ class TelegramUser(models.Model):
 	def __str__(self):
 		return self.login
 
-
 class Task(models.Model):
+	STATUS_VARIANTS = [
+		(1, None),
+		(2, 'rejected'),
+		(3, 'acepted'),
+		(4, 'complited'),
+	]
+
 	name = models.CharField(max_length=300)
 	description = models.TextField(default='No description')
 	date = models.DateTimeField(default=timezone.now)
 	user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE)
-	acceed = models.BooleanField(null=True, blank=True, default=None)
-	complited = models.BooleanField(default=False)
-	
+	status = models.IntegerField(choices=STATUS_VARIANTS, default=1)	
+
 	def __str__(self):
 		return f'{self.name} for {self.user} on {self.date}'

d508a4364815d8bd62bb7647624f27d9c5045880
Peteliuk <43549721+Peteliuk@users.noreply.github.com>
2020-02-13 00:53:19 +0200

    Update models.py

--- bot/models.py
+++ bot/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class TelegramUser(models.Model):
-	tg_id = models.CharField(max_length=200, null=True, blank=True)
+	tg_id = models.CharField(max_length=200, blank=True, default='')
 	login = models.CharField(max_length=200, unique=True)
 	password = models.CharField(max_length=200)
 

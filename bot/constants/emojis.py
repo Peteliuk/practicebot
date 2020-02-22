715269e428fec316537fb47a64e5a8728985bae5
Peteliuk <43549721+Peteliuk@users.noreply.github.com>
2020-02-22 12:23:48 +0200

    Add constants

---
+++ bot/constants/emojis.py
@@ -0,0 +1,10 @@
+from emoji import emojize
+
+
+# Emojis
+grin = emojize(':grin:', use_aliases=True)
+slightly_smiling_face = emojize(':slightly_smiling_face:', use_aliases=True)
+x = emojize(':x:', use_aliases=True)
+white_check_mark = emojize(':white_check_mark:', use_aliases=True)
+sign_of_the_horns = emojize(':sign_of_the_horns:', use_aliases=True)
+confused = emojize(':confused:', use_aliases=True)
---
+++ bot/constants/strings.py
@@ -0,0 +1,14 @@
+# Bot token
+TOKEN = 'TOKEN'
+
+# Commands in message handlers
+start_cmd = ['start', 'Start']
+help_cmd = ['help', 'Help']
+tasks_cmd = ['tasks', 'Tasks']
+remove_keyboard_cmd = ['rmkeyboard']
+
+# Commands in callback query handlers
+future_tasks = 'future tasks'
+past_tasks = 'past tasks'
+reject = 'reject'
+accept = 'accept'
